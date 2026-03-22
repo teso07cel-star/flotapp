8098de0e93d32d3d3eca8ca2d82b956f88ea9199
Flotapp Admin <admin@flotapp.com>
2026-03-21 23:20:35 -0300

    Subida inicial automatizada

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -8,7 +8,11 @@ const databaseUrl =
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    datasourceUrl: databaseUrl,
+    datasources: {
+      db: {
+        url: databaseUrl,
+      },
+    },
   })
 }
 
