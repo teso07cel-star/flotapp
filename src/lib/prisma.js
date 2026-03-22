3de5e822ee858df3a0d590dd8974e2c4f352edfb
Flotapp Admin <admin@flotapp.com>
2026-03-21 23:16:26 -0300

    Subida inicial automatizada

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
+const databaseUrl = 
+  process.env.POSTGRES_URL || 
+  process.env.POSTGRES_PRISMA_URL || 
+  process.env.POSTGRES_PRISMA_DATABASE_URL ||
+  process.env.DATABASE_URL;
+
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    datasourceUrl: databaseUrl,
+  })
 }
 
 const globalForPrisma = globalThis
