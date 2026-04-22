7087cba0e0e551344e17d36a2a8f267f3b4d3892
Flotapp Admin <admin@flotapp.com>
2026-04-22 20:58:28 -0300

    fix: Compatibilidad con Prisma 7 - Moviendo config a prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
   datasource: {
-    // URL Directa blindada para asegurar que Migrate y Generate funcionen sin el proxy
-    url: 'postgresql://postgres.siqxydghsjmvmjgkmvps:admin123@db.siqxydghsjmvmjgkmvps.supabase.co:5432/postgres',
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
   },
 });
