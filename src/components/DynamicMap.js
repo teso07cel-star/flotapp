12a06c4ed3a4ea356bd8f1daa656366071233d5f
teso07cel-star <teso07cel@gmail.com>
2026-05-04 00:01:41 -0300

    Update DynamicMap.js

--- src/components/DynamicMap.js
+++ src/components/DynamicMap.js
@@ -1,1 +1,1 @@
-export default function DynamicMap() { return <div className="p-4 bg-gray-800 text-gray-400 rounded">Mapa desactivado temporalmente.</div>; }
+export default function DynamicMap() { return <div className="p-4 bg-gray-800 text-gray-400 rounded border border-blue-500/30">Mapa desactivado temporalmente.</div>; }
